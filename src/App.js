8bbe2f9b9130e0001c4e40fe7e7687b2e1e17899
yain09 <yain09@gmail.com>
2023-08-23 12:03:37 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/largo.svg";
+import "./App.css";
+import { Navbar, Container } from "react-bootstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar className="bg-black">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              height="50"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
---
+++ src/script/components/tarjeta.jsx
@@ -0,0 +1,56 @@
+let sheetId = '/1jCpaUtRcak3yeYa8YvzyLNg36UCxYrq9lX8AsDFRnTs';
+let sheetTitle = 'Cronograma';
+
+let fullUrl = ('https://docs.google.com/spreadsheets/d' + sheetId + '/gviz/tq?sheet=' + sheetTitle);
+
+class miClase {
+    constructor(fecha,clase,ut,teoria,practica,tp,docente){
+        this.fecha = fecha;
+        this.clase = clase;
+        this.ut = ut;
+        this.teoria = teoria;
+        this.practica = practica;
+        this.tp = tp;
+        this.docente = docente;
+        
+    }
+    tarjeta(){
+        document.write(`<article class="col"> <div class="card h-100 shadow-sm"> <div class="card-header text-center bg1 border"> <div class="row"> <div class="col align-self-center"> <p class="m-0">Fecha</p> <p class="h5" id="idFecha">${this.fecha}</p> </div> <div class="col col align-self-center"> <p class="m-0">Clase N°</p> <p class="h5" id="idClase">${this.clase}</p> </div> <div class="col col align-self-center"> <p class="m-0">U. Temática</p> <p class="h5" id="idUnidad">${this.ut}</p> </div> </div> </div> <div class="card-body"> <h5 class="card-title">Teoría</h5> <div class="card-text" id="idTeoria">${this.teoria}</div> <h5 class="card-title mt-2">Práctica</h5> <div class="card-text" id="idPractica">${this.practica}</div> <h5 class="card-title mt-2">TP</h5> <div class="card-text" id="idTp">${this.tp}</div> </div> <div class="card-footer"> <p class="text-black-50" id="idDocente">${this.docente}</p> </div> </div> </article>`)
+    }
+}
+
+fetch(fullUrl)
+    .then(res => res.text())
+    .then(rep => {
+        var data = JSON.parse(rep.substr(47).slice(0,-2));
+   
+        let idFecha = document.getElementById('idFecha');
+        let idClase = document.getElementById('idClase');
+        let idUnidad = document.getElementById('idUnidad');
+        let idTeoria = document.getElementById('idTeoria');
+        let idPractica = document.getElementById('idPractica');
+        let idTp = document.getElementById('idTp');
+        let idDocente = document.getElementById('idDocente');
+        let length = data.table.rows.length;
+        let tabla = (data.table.rows)
+
+        for(var i = 0;i<=0;i++){
+            let fila = tabla[i].c;
+            let info = new miClase(fila[0].f,fila[1].f,fila[2].f,fila[5].v,fila[7].v,fila[9].v,fila[3].v);
+            // info.tarjeta()
+            
+        // idFecha.innerHTML = fila[0].f;
+        // idClase.innerHTML = fila[1].f;
+        // idUnidad.innerHTML = fila[2].f;
+        // idTeoria.innerHTML = fila[5].v;
+        // idPractica.innerHTML = fila[7].v;
+        // idTp.innerHTML = fila[9].v;
+        // idDocente.innerHTML = fila[3].v;
+            
+        }
+ 
+            
+})
+
+const primera = new miClase("00 ABR","01","1","Estructuras reticuladas:Estructuras de acero. Estructuras de madera Estudio isostático. Solicitaciones y esfuerzos de los componentes estructurales sometidos a cargas estáticas verticales y oblicuas. Métodos gráficos y analíticos.","Ejercitación sobre teoría","Seguimiento TP2","Valeria Herrero");
+primera.tarjeta();
